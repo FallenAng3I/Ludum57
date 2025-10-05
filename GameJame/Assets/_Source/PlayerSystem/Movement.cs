3cf23d86fdf99f8fdaf2d139916d8d03ffdc2b02
Eugen <s4mararunner@mail.ru>
2025-10-05 13:53:58 +0300

    feat: movement

---
+++ GameJame/Assets/_Source/PlayerSystem/Movement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        rb.velocity = movement;
+    }
+}
